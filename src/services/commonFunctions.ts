69606f8a5153bfc9a5b8cd9dc90d6260f800dcb7
Manos-Kar <manos@eperoto.com>
2024-07-22 18:39:04 +0300

    fixes getImageLink

--- src/services/commonFunctions.ts
+++ src/services/commonFunctions.ts
@@ -5,5 +5,5 @@ export const getImageLink = (imageUrl: string) => {
 
   return isDevEnv()
     ? `http://localhost:8000${imageUrl}`
-    : `https://www.manoskarystinos.com/var/www/manos-movies/movies-backend${imageUrl}`;
+    : `https://www.manoskarystinos.com/media/${imageUrl}`;
 };
