a8bee5987b8f821a325bd3d0402e8fc8d2280d9c
Manos-Kar <manos@eperoto.com>
2024-07-22 18:18:10 +0300

    fixes update-server

--- src/services/commonFunctions.ts
+++ src/services/commonFunctions.ts
@@ -1,6 +1,8 @@
 export const isDevEnv = () => process.env.REACT_APP_ENV === "dev";
 
 export const getImageLink = (imageUrl: string) => {
+  console.log(imageUrl);
+
   return isDevEnv()
     ? `http://localhost:8000${imageUrl}`
     : `https://www.manoskarystinos.com/var/www/manos-movies/movies-backend${imageUrl}`;
